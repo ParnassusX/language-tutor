e71ed7b6a28dfef3d689a3d47f14a11ca8ad1ea9
WorthIt <imaginariumlabs@example.com>
2025-07-07 01:32:18 +0200

    fix: vercel build

---
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.CVRQZei-.js","_app/immutable/chunks/CWj6FrbW.js","_app/immutable/chunks/C5xkfXMJ.js","_app/immutable/chunks/L7fnnnqL.js","_app/immutable/chunks/ui5THt8X.js","_app/immutable/chunks/IM8332wN.js","_app/immutable/chunks/Bkusq2lv.js","_app/immutable/chunks/DRA-aPd3.js"];
+export const stylesheets = [];
+export const fonts = [];
